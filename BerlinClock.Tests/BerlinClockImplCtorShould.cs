902520e135050b4aeed8478789cfb7aa41d6671e
epasmam <epasmam527@gmail.com>
2020-02-15 20:36:05 +0100

    Initial commit

---
+++ BerlinClock.Tests/BerlinClockImplCtorShould.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BerlinClock.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BerlinClock.Tests
+{
+    [TestClass]
+    public class BerlinClockImplCtorShould
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [DataRow(null)]
+        [DataRow("")]
+        public void ThrowArgumentNullExceptionIfctorParameterIsNullOrEmpty(String emptyTime)
+        {
+            // Arrange, Act
+            var clock = new BerlinClockImpl(emptyTime);
+        }
+    }
+}
---
+++ Classes/BerlinClockImpl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerlinClock.Classes
+{
+    public class BerlinClockImpl
+    {
+        private Int32 _timeInSeconds;
+
+        private const string IncorrectArgumentErrorMessage = "inputTime parameter should contain correct time";
+
+        private const Char Yellow = 'Y';
+        private const Char Red = 'R';
+        private const Char Empty = 'O';
+
+        public BerlinClockImpl(string inputTime)
+        {
+            if (String.IsNullOrWhiteSpace(inputTime))
+                throw new ArgumentNullException(nameof(inputTime), IncorrectArgumentErrorMessage);
+
+            if (DateTime.TryParse(inputTime, out DateTime parsedInputTime) == false)
+                throw new ArgumentException(nameof(inputTime), IncorrectArgumentErrorMessage);
+
+            this._timeInSeconds = (Int32)parsedInputTime.TimeOfDay.TotalSeconds;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
